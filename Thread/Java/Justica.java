50dab6752a26bd02b40e64c3a9584759e406bf3f
F4NT0 <gabriel.stundner@acad.pucrs.br>
2020-05-03 19:06:47 -0300

    programas concorrentes

---
+++ Thread/Java/Justica.java
@@ -0,0 +1,25 @@
+/****************************
+* EXEMPLO DE JUSTIÇA EM JAVA
+****************************/
+
+class Justica extends Thread {
+    static volatile int n = 1;
+
+    public void run() {
+        System.out.println("Tentando!");
+        n=-1;
+        System.out.println("Justo!");
+    }
+
+    public static void main(String[] args) {
+      Justica p = new Justica();
+      p.start();
+
+      while (n>0) {
+		  n++;
+		  System.out.println(".");
+      }
+
+      System.out.println("Fim");
+    }	
+}
