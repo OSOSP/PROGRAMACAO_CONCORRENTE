a024a792335c0de8b6ed56bf12279220c55fa245
F4NT0 <gabriel.stundner@acad.pucrs.br>
2020-06-25 16:41:41 -0300

    Algoritmo de Peterson

---
+++ Java/Algoritmo-Peterson/Peterson.java
@@ -0,0 +1,33 @@
+public class Peterson implements Runnable { 
+    private static boolean[] in = { false, false }; 
+    private static volatile int turn = -1; 
+    private final int id;
+
+    // Construtor
+    public Peterson(int i) { 
+        id = i; 
+    } 
+
+    // Função Private
+    private int other() { 
+        return id == 0 ? 1 : 0; 
+    }
+    
+
+    // Método Run de Threads
+    @Override 
+    public void run() { 
+        in[id] = true; turn = other(); 
+        while (in[other()] && turn == other()) { 
+            System.out.println("[" + id + "] - Waiting..."); 
+        } 
+        System.out.println("[" + id + "] - Working (" + ((!in[other()]) ? "other done" : "my turn") + ")"); 
+        in[id] = false; 
+    }
+
+    public static void main(String[] args) {
+        new Thread(new Peterson(0), "Thread - 0").start(); 
+        new Thread(new Peterson(1), "Thread - 1").start(); 
+    } 
+      
+} 
